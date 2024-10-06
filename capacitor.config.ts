c5de096cfb88cf5c6624d1b14c9782be200efa45
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-10-06 13:21:37 +0200

    primer_commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_tema2_ejercicio1_recetario',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
